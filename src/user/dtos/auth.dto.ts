db507ac9cb491f40ca68486a8820fa903bc34538
Bibin Lopez <bibinlopez36@gmail.com>
2023-07-21 17:51:30 +0530

    auth_dto

---
+++ src/user/dtos/auth.dto.ts
@@ -0,0 +1,26 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  Matches,
+} from 'class-validator';
+import {} from 'class-transformer';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Matches(/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$ /, {
+    message: 'phone must be a valid phone number',
+  })
+  phone: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(5)
+  password: string;
+}

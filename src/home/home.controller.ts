33e3f6a63bfe541c995c8b71f7d4940a3c9214e8
Bibin Lopez <bibinlopez36@gmail.com>
2023-07-22 16:33:05 +0530

    get all Homes

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { HomeService } from './home.service';
 
 @Controller('home')
 export class HomeController {
+  constructor(private readonly homeService: HomeService) {}
   @Get()
   getHomes() {
-    return 'get homes';
+    return this.homeService.getHomes();
   }
 
   @Get('/:id')

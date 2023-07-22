90e3c2e617e847467e9b56d03abdef8f6b65aa4a
Bibin Lopez <bibinlopez36@gmail.com>
2023-07-22 18:44:32 +0530

    filter-queryParams

--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HomeResponseDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
+import { NotFoundError } from 'rxjs';
+
+interface GetHomesParam {
+  city?: string;
+  price?: {
+    gte?: number;
+    lte?: number;
+  };
+  propertyType?: PropertyType;
+}
 
 @Injectable()
 export class HomeService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async getHomes(): Promise<HomeResponseDto[]> {
+  async getHomes(filter: GetHomesParam): Promise<HomeResponseDto[]> {
     const homes = await this.prismaService.home.findMany({
       select: {
         id: true,
@@ -23,7 +34,15 @@ export class HomeService {
           take: 1,
         },
       },
+      where: {
+        ...filter,
+      },
     });
+
+    if (!homes.length) {
+      throw new NotFoundException();
+    }
+
     return homes.map((home) => {
       const fetchHome = { ...home, image: home.images[0].url };
       delete fetchHome.images;
